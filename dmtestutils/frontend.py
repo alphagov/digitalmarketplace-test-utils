50792a54fad533a8ec6158cdc196149ed4321188
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2018-03-26 17:37:15 +0100

    add initial frontend.py, including basic BaseFrontendApplicationTest

---
+++ dmtestutils/frontend.py
@@ -0,0 +1,8 @@
+from flask import session
+from markupsafe import escape
+
+
+class BaseFrontendApplicationTest(object):
+    def get_flash_messages(self):
+        with self.client.session_transaction() as session:
+            return tuple((category, escape(message)) for category, message in (session.get("_flashes") or ()))
