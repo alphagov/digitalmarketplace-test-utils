5c211b44f1008429234b310f257847e263fe61a8
Kat Stevens <katherine.stevens@digital.cabinet-office.gov.uk>
2019-01-14 15:47:41 +0000

    API stub for BriefResponse model

--- dmtestutils/api_model_stubs/__init__.py
+++ dmtestutils/api_model_stubs/__init__.py
@@ -1,6 +1,7 @@
 from .base import BaseAPIModelStub
 from .audit_event import AuditEventStub
 from .brief import BriefStub
+from .brief_response import BriefResponseStub
 from .framework import FrameworkStub
 from .framework_agreement import FrameworkAgreementStub
 from .lot import LotStub, as_a_service_lots, cloud_lots, dos_lots
@@ -9,13 +10,6 @@ from .supplier_framework import SupplierFrameworkStub
 
 
 # TODO: Flesh out the stubs below and move to their own modules
-
-
-class BriefResponseStub(BaseAPIModelStub):
-    resource_name = 'briefResponses'
-    default_data = {
-
-    }
 
 
 class DirectAwardProjectStub(BaseAPIModelStub):
